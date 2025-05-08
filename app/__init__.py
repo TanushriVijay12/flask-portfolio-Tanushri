6c9bd2f1f75fee370e6193efab222ad9be5972a6
TanushriVijay12 <tanushri.fall24@gmail.com>
2025-05-07 23:19:39 -0400

    Updated templates location

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
 
-# extensions
-db = SQLAlchemy()
-migrate = Migrate()
-login = LoginManager()
-login.login_view = 'admin.login'
-
-# application factory
 def create_app(config_class=None):
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
 
-    # init extensions
-    db.init_app(app)
-    migrate.init_app(app, db)
-    login.init_app(app)
-
-    # register blueprints
+    # Import and register blueprint
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
